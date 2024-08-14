07ddb0b3cb09f0bb5176335a446ae75e71bf0d53
DziShy <dshydlouskaya@proton.me>
2024-08-14 14:42:44 +0200

    added Closure

---
+++ course-40-challenging-Pyton-programs/7-Functions/Closure.py
@@ -0,0 +1,59 @@
+# 1
+def outer():        # внешняя функция
+    n = 5           # лексическое окружение - локальная переменная
+ 
+    def inner():      # локальная функция
+        nonlocal n
+        n += 1        # операции с лексическим окружением
+        print(n)
+ 
+    return inner
+  
+fn = outer()   # fn = inner, так как функция outer возвращает функцию inner
+# вызываем внутреннюю функцию inner
+fn()    # 6
+fn()    # 7
+fn()    # 8
+
+
+
+#2 
+def multiply(n):
+    def inner(m): return n * m
+ 
+    return inner
+  
+fn = multiply(5)
+print(fn(5))        # 25
+print(fn(6))        # 30
+print(fn(7))        # 35
+
+
+#3
+def multiply(n): return lambda m: n * m
+
+fn = multiply(5)
+print(fn(5))        # 25
+print(fn(6))        # 30
+print(fn(7))        # 35
+
+
+#4
+
+def makeCounter():
+    n = 0
+    def inner ():
+        nonlocal n
+        n +=1
+        return n
+    return inner
+
+counter = makeCounter()
+print(counter())  # Вывод: 1
+print(counter())  # Вывод: 2
+print(counter())  # Вывод: 3
+
+
+new_counter = makeCounter()
+print(new_counter())  # Вывод: 1
+print(new_counter())  # Вывод: 2
